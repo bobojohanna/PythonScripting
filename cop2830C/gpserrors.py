1da0113a3cca1990334b2a932d7b1624c9f19b50
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-10-12 20:45:37 -0400

    Update gpserrors.py

--- cop2830C/gpserrors.py
+++ cop2830C/gpserrors.py
@@ -2,8 +2,8 @@ import csv
 import matplotlib.pyplot as plt
 x=[]
 y=[]
-with open('/Users/jj/Downloads/gps_day.csv', 'r') as cvsfile:
-    lines = csv.reader(cvsfile, delimiter= ',')
+with open('/Users/jj/Downloads/gps_day.csv', 'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter= ',')
     for row in lines:
        x.append(row[0])
        y.append(float(row[1]))
